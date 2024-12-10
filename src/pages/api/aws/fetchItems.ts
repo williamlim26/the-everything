1b51a9c0ff1c4808cfbb62648fe2f932e489afe6
William Lim <william.lim2@telus.com>
2024-12-09 16:39:09 -0800

    fix env varibales

--- src/pages/api/aws/fetchItems.ts
+++ src/pages/api/aws/fetchItems.ts
@@ -8,20 +8,14 @@ export default async function fetchItems(
 ) {
   const { id = undefined, shortUrl = undefined } = req.query
   // Fetching AWS credentials and region from environment variables
-  const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env
+  // const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env
 
-  // Check if environment variables are present
-  if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY || !AWS_REGION) {
-    throw new Error('Missing AWS environment variables')
-  }
+  // // Check if environment variables are present
+  // if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY || !AWS_REGION) {
+  //   throw new Error('Missing AWS environment variables')
+  // }
 
-  const client = new DynamoDBClient({
-    region: AWS_REGION,
-    credentials: {
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    },
-  })
+  const client = new DynamoDBClient({})
   const docClient = DynamoDBDocumentClient.from(client)
 
   const command = new ScanCommand({
@@ -34,10 +28,9 @@ export default async function fetchItems(
   try {
     const response = await docClient.send(command)
     console.log('Successfully fetched items:', response)
-    return res.json(response)
+    return res.status(200).json(response)
   } catch (error) {
     console.error('Error fetching items:', error)
-    console.error('AWS_ACCESS_KEY_ID:', AWS_ACCESS_KEY_ID)
     return res.status(500).json({
       message: 'Error fetching items',
       error,
